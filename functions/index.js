625875956f840062d0dac807b5c87faaa81a8278
Dan Richardson <dan.richardson.gs@gmail.com>
2021-05-20 23:53:40 +1000

    Basic barcode search working

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,128 @@
 const functions = require("firebase-functions");
-const axios = require('axios');
+const EbayAuthToken = require("ebay-oauth-nodejs-client");
+const axios = require("axios");
+const CircularJSON = require('circular-json');
+const env = functions.config();
 
-const toStrip = ['ultra hd', 'ultra-hd', 'uhd', 'blu ray', 'blu-ray', 'bluray', 'dvd', '4k']
+const ebayScopes = ["https://api.ebay.com/oauth/api_scope", "https://api.ebay.com/oauth/api_scope/sell.inventory"];
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest(async (req, resp) => {
-    
-    try 
+const ebayAuthToken = new EbayAuthToken({
+  clientId: env.ebay.appid,
+  clientSecret: env.ebay.certid,
+  redirectUri: env.ebay.runame
+});
+
+const toStrip = ["ultra hd", "ultra-hd", "uhd", "blu ray", "blu-ray", "bluray", "dvd", "4k"];
+
+// https://stackoverflow.com/a/7313467
+String.prototype.replaceAll = function(strReplace, strWith)
+{
+  const esc = strReplace.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+  const reg = new RegExp(esc, 'ig');
+  return this.replace(reg, strWith);
+};
+
+exports.helloWorld = functions.https.onRequest(async (req, resp) =>
+{
+  try
+  {
+    const barcode = req.query.barcode;
+    const tokenResp = await ebayAuthToken.getApplicationToken('PRODUCTION', ['https://api.ebay.com/oauth/api_scope']);
+    const headers = {
+      'Authorization': `Bearer ${CircularJSON.parse(tokenResp).access_token}`,
+      'X-EBAY-C-MARKETPLACE-ID' : req.query.region ? req.query.region : 'EBAY_US'
+    };
+
+    let ebayResp = await axios.get(`https://api.ebay.com/buy/browse/v1/item_summary/search?gtin=${barcode}`, {'headers':headers});
+
+    if (!ebayResp || !ebayResp.data || ebayResp.data.total == 0)
     {
-        // https://developer.ebay.com/DevZone/build-test/test-tool/?index=0&env=production&api=browse&call=item_summary_search__GET&variation=json
-        //https://api.ebay.com/buy/browse/v1/item_summary/search?gtin=5051892230087
-
-        var barcode = req.query.barcode;    
-        var appId = "let's not";
-        var lastData = {}
-
-        var ebayResp = await axios.get(`https://open.api.ebay.com/shopping?version=1119&appid=${appId}&callname=FindProducts&ProductID.Value=${barcode}&ProductID.Type=UPC&ResponseEncodingType=JSON`)
-
-        if(!ebayResp || !ebayResp.data.Product || ebayResp.data.Product.length === 0)
-        {
-            console.log("Trying again...")
-            ebayResp = await axios.get(`https://open.api.ebay.com/shopping?version=1119&appid=${appId}&callname=FindProducts&QueryKeywords=${barcode}&ResponseEncodingType=JSON`)
-        }
-
-        if(!ebayResp || !ebayResp.data.Product || ebayResp.data.Product.length === 0)
-        {
-            console.log(ebayResp.data)
-            return resp.send("Not found");
-        }
-
-        lastData = ebayResp.data.Product[0];
-
-        // strip of extra descriptors in brackets e.g. (4k UHD, 2019)
-        var title = ebayResp.data.Product[0].Title.replace(/ *\([^)]*\) */g, "");
-        var omdb = 'nice try';
-
-        var omdbResp = await axios.get(`https://www.omdbapi.com/?t=${title}&apikey=${omdb}`)
-        
-        if(omdbResp && omdbResp.data)
-        {
-            return resp.send(omdbResp.data);
-        }
-        return resp.send(lastData);
-    } 
-    catch (error) {
-        console.log(error);
-        return resp.send(error);
+      console.log("Trying again...");
+      ebayResp = await axios.get(`https://api.ebay.com/buy/browse/v1/item_summary/search?q=${barcode}`, {'headers':headers});
     }
 
-    //http://open.api.ebay.com/shopping?callname=FindProducts­&responseencoding=XML­&appid=AppID­&siteid=0­&version=663­&ProductID.Type=UPC­&ProductID.Value=711719866824
-  });
+    if (!ebayResp || !ebayResp.data || ebayResp.data.total == 0)
+    {
+      return resp.send("Not found");
+    }
+
+    const lastData = ebayResp.data.itemSummaries[0];
+
+    // strip of extra descriptors in brackets e.g. (4k UHD, 2019)
+    lastData.title = lastData.title.replace(/ *\([^)]*\) */g, " ");
+    // strip stuff in asterisks e.g. *NEW - SEALED*
+    lastData.title = lastData.title.replace(/ *\*[^)]*\* */g, " ");
+    // strip format names
+    toStrip.forEach((s) =>
+    {
+      lastData.title = lastData.title.replaceAll(s, ' ');
+    });
+
+    lastData.title.trim();
+
+    const omdbResp = await axios.get(`https://www.omdbapi.com/?t=${lastData.title}&apikey=${env.omdb.key}`);
+
+    if (omdbResp && omdbResp.data && omdbResp.data.Response)
+    {
+      return resp.send({"success" : true, "data" : omdbResp.data});
+    }
+
+    return resp.send({"success" : false, "data" : lastData});
+  }
+  catch (error)
+  {
+    console.log(error);
+    return resp.send(error);
+  }
+});
+
+// Currently unused
+exports.ebayOauthGetUrl = functions.https.onRequest(async (req, resp) =>
+{
+  try
+  {
+    const authUrl = ebayAuthToken.generateUserAuthorizationUrl("PRODUCTION", ebayScopes);
+    resp.send(authUrl);
+  }
+  catch (err)
+  {
+    return resp.status(500).send(err);
+  }
+});
+
+// Currently unused
+exports.ebayOauthGetTokens = functions.https.onRequest(async (req, resp) =>
+{
+  try
+  {
+    if (!req.query.ebaycode)
+    {
+      return resp.status(400).send("Ebay code required");
+    }
+
+    const accessToken = await ebayAuthToken.exchangeCodeForAccessToken("PRODUCTION", req.query.ebaycode);
+    resp.send(accessToken);
+  }
+  catch (err)
+  {
+    return resp.status(500).send(err);
+  }
+});
+
+exports.ebayOathRefreshToken = functions.https.onRequest(async (req, resp) =>
+{
+  try
+  {
+    if (!req.query.refreshToken)
+    {
+      return resp.status(400).send("Ebay refresh token required");
+    }
+
+    const accessToken = await ebayAuthToken.getAccessToken("PRODUCTION", req.query.refreshToken, ebayScopes);
+    resp.send(accessToken);
+  }
+  catch (err)
+  {
+    return resp.status(500).send(err);
+  }
+});
